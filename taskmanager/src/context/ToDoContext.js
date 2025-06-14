d79445d8317274b6d7d733aef614c98dc18f1b58
hanyucrocks <sakshiseksy@gmail.com>
2025-06-15 02:01:50 +0530

    task manager done

---
+++ taskmanager/src/context/ToDoContext.js
@@ -0,0 +1,24 @@
+// creat context// use context
+
+import {createContext, useContext} from "react"
+
+export const ToDoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: " Todo msg",
+            completed: false,
+        }
+    ],
+    addTodo: (todo) => {},
+    updatedTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+export const useTodo = () => {
+    return useContext(ToDoContext)
+}
+
+export const TodoProvider = ToDoContext.Provider
+
